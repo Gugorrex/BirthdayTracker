6daa0851b8c631266e89b265caa72dabdc61e97c
Gugorrex <tobi.buettgen@web.de>
2021-12-02 18:05:00 +0100

    added Model.java (Singleton)

---
+++ src/main/java/gugorrex/model/Model.java
@@ -0,0 +1,12 @@
+package gugorrex.model;
+
+public class Model {
+    private static Model instance;
+
+    public static Model getInstance() {
+        if (instance == null) {
+            instance = new Model();
+        }
+        return instance;
+    }
+}
